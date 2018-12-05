f4dbacddf6672163ff35f1357556e93afdab061a
dj6024 <814476587@qq.com>
2018-12-05 06:30:23 -0800

    初步构建结构

--- tao/urls.py
+++ tao/urls.py
@@ -4,7 +4,12 @@ from tao import views
 
 urlpatterns = [
     # 首页
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='index'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^market/$', views.market, name='market'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^mine/$', views.mine, name='mine'),
+
 
 ]
 
--- tao/views.py
+++ tao/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 
-def index(request):
+def home(request):
     return render(request, 'home/home.html')
 
+
+def market(request):
+    return render(request, 'market/market.html')
+
+
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+
+def mine(request):
+    return render(request, 'mine/mine.html')
 
 
